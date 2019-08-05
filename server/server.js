5025f7692b2617eb14a84c3aba928eb585c77389
Manux <burban.dh@gmail.com>
2019-08-05 18:14:30 -0400

    Agregamos los controladores y models del usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,45 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//Importamos el archivo de usuarios.js
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('BBDD online');
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-
-    } else {
-        res.json({
-            persona: body,
-            text: 'post Usuario'
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id,
-        text: "put con paranms"
-    })
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto', process.env.PORT);
-})
+});
